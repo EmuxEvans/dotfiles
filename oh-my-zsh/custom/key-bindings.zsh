515a60c00cd11033eacbbba48d78c5f1213a81a7
JIANG Di <jiang-di@outlook.com>
2016-01-21 14:27:19 +0800

    clean up

--- oh-my-zsh/custom/key-bindings.zsh
+++ oh-my-zsh/custom/key-bindings.zsh
@@ -1,8 +1,2 @@
-# Add yourself some shortcuts to projects you often work on
-# Example:
-#
-# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
-#
-
 bindkey -s '\eg' 'gst\n'                               # [Alt-g] - run command: gst
 bindkey -s '\e^[[A' 'cd ../\n'                               # [Alt-up] - run command: cd ../
