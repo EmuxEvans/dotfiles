199da7d40cf723a855f72b2f4c45f1aa50cfe469
JIANG Di <jiang-di@outlook.com>
2016-01-21 14:25:42 +0800

    add some key bindings

---
+++ oh-my-zsh/custom/key-bindings.zsh
@@ -0,0 +1,8 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+
+bindkey -s '\eg' 'gst\n'                               # [Alt-g] - run command: gst
+bindkey -s '\e^[[A' 'cd ../\n'                               # [Alt-up] - run command: cd ../
